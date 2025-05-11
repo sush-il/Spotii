377469565ffd54f9aab8b31bf6284f4717f8f903
sush-il <zoroat3@gmail.com>
2025-05-11 20:41:28 +0100

    PNPM SWITCH

--- scantest.js
+++
@@ -1,4 +0,0 @@
-// test-secrets.js
-const awsKey = "AKIA1234567890EXAMPLE";
-const slackWebhook = "https://hooks.slack.com/services/T00000000/B00000000/XXXXXXXXXXXXXXXXXXXXXXXX";
-const githubToken = "ghp_1234567890abcdefghijklmnopqrstuvwxyzABCD";
